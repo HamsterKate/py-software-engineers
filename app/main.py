a14bac2ca4b6f2a5d852870e6b18500cd2194562
Kate <hamster.k.po@gmail.com>
2026-02-18 22:48:56 +0200

    FullSatck init with skills oreder

--- app/main.py
+++ app/main.py
@@ -38,6 +38,11 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        SoftwareEngineer.__init__(self, name)
+        BackendDeveloper.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
